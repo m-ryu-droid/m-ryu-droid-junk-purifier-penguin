1729874ea560b8e4952a3553f7be243ab7257c6b
m-ryu-droid <m-ryu@co-success.jp>
2026-05-15 22:59:31 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// APIキーや目標値などの設定
+const part1 = "AIza"; 
+const part2 = "SyCVm1ZdSOBR5q8gnDJl9RFB15b_cwq4dJU"; 
+const API_KEY = part1 + part2;
+const BOSS_GOAL = 30;
+
+// アイテムリスト
+const items = [
+    { name: 'ニット帽', pt: 10, img: 'hat_straw.png.png', type: 'hat' },
+    { name: 'サングラス', pt: 30, img: 'glasses.png', type: 'hat' },
+    { name: '勇者のマント', pt: 50, img: 'mantle.png', type: 'body' }
+];
